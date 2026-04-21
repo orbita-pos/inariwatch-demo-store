3564cd155e46f03186a93242b02c2871b1d3544e
Jesus Bernal <bernal.rojas.dev@gmail.com>
2026-04-20 19:32:47 -0700

    chore(fixtures): inject bug undefined-foreach for golden dataset

---
+++ __inari_bugs_fixtures__/bug-02-foreach-undefined.js
@@ -0,0 +1,6 @@
+// Bug: data may be undefined when API returns empty
+module.exports = function sumItems(data) {
+  let total = 0;
+  data.forEach(item => { total += item.value; });
+  return total;
+};
