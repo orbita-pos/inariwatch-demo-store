138a6406e26bc5165eccf961b215a903dfe35a9d
Jesus Bernal <bernal.rojas.dev@gmail.com>
2026-04-20 20:07:05 -0700

    chore(fixtures): inject bug division-by-zero for golden dataset

---
+++ __inari_bugs_fixtures__/bug-04-div-zero.js
@@ -0,0 +1,6 @@
+// Bug: no check for count=0 results in Infinity
+module.exports = function average(values) {
+  const count = values.length;
+  const sum = values.reduce((a, b) => a + b, 0);
+  return sum / count;
+};
