9be4439f7a018d1ad9d709345270d4466ec3eb18
Jesus Bernal <bernal.rojas.dev@gmail.com>
2026-05-03 00:24:13 -0700

    fix: TypeError: Cannot read property 'name' of undefined [pr19-mo
    Automated fix by Inari AI (attempt 2)

--- __inari_bugs_fixtures__/bug-01-null-deref.js
+++ __inari_bugs_fixtures__/bug-01-null-deref.js
@@ -1,5 +1,4 @@
 // Bug: user.profile can be undefined
 module.exports = function getUserName(user) {
-  const profile = user.profile || {};
-  return String(profile.name || '').toUpperCase();
+  return user?.profile?.name?.toUpperCase() ?? "";
 };
--- __tests__/bug-01-null-deref.test.js
+++ __tests__/bug-01-null-deref.test.js
@@ -1,7 +1,7 @@
 const getUserName = require('../__inari_bugs_fixtures__/bug-01-null-deref');
 
 describe('bug-01 null dereference regression', () => {
-  test('returns an empty string when user.profile is undefined', () => {
+  test('returns an empty string when user.profile is undefined instead of throwing', () => {
     expect(() => getUserName({ id: 1 })).not.toThrow();
     expect(getUserName({ id: 1 })).toBe('');
   });
